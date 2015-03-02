f921d6d1b0bdb309bd16d486a256b579333a5f68
BorislavaJ <borislava_ch@abv.bg>
2015-03-02 20:29:08 +0200

    Adding class Upgrade

--- AlienInvaders/AlienInvaders/Upgrade.cs
+++ AlienInvaders/AlienInvaders/Upgrade.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVersion1
+{
+    class Upgrade
+    {
+        public int x;
+        public int y;
+        public int direction;
+        public int secondDirection;
+        public int type;
+
+        public Upgrade(int xValue, int yValue, int d, int t, int sd = 0)
+        {
+            x = xValue;
+            y = yValue;
+            direction = d;
+            secondDirection = sd;
+            type = t;
+        }
+    }
+}
