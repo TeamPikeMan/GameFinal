099be95b28e30ae7cf9e673a860db156a863a7c7
PavelNPavlov <pavelnapavlov@gmail.com>
2015-03-02 13:06:54 +0200

    GameBodyUpdate

--- AlienInvaders/AlienInvaders/GameBody.cs
+++ AlienInvaders/AlienInvaders/GameBody.cs
@@ -3,13 +3,230 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
-namespace AlienInvaders
+namespace GameVersion1
 {
-    class GameBody
+    partial class GameBody
     {
+        public static Hero player = new Hero();
+
+        struct Score
+        {
+            public string name;
+            public int points;
+            public DateTime date;
+
+            public Score(string playerName, int p, DateTime dateTime)
+            {
+                this.name = playerName;
+                this.points = p;
+                this.date = dateTime;
+            }
+
+            public string ToString()
+            {
+                return name + "-" + points.ToString() + "-" + date.ToString();
+            }
+        }
         static void Main(string[] args)
         {
+            int h = 29;
+            int w = 80;
+            int tbBorder = 4;
+            ConsoleSetUp(w, h);
+            DrawBackGround(h, w);
+            
+            List<Alien> swarm = new List<Alien>();
+            List<Projectile> missles = new List<Projectile>();
+            List<Upgrade> upgrades = new List<Upgrade>();
+            Random rng = new Random();
+            int[,] matrix = new int[h - 2 * tbBorder, w / 2];
+
+            string[] menuText = { "Start", "Options", "HighScore", "Exit" };
+
+        Selection:
+            int select = 0;
+            PrintMenu(select, menuText, w);
+
+            while (true)
+            {
+                
+                ConsoleKeyInfo userInput = Console.ReadKey();
+                if (userInput.Key == ConsoleKey.UpArrow && select >= 1)
+                {
+                    select--;
+                }//move up selection
+                if (userInput.Key == ConsoleKey.DownArrow && select <= menuText.Length - 2)
+                {
+                    select++;
+                } //move down selection
+
+                if (userInput.Key == ConsoleKey.Enter)
+                {
+                    break; //stop selection process
+                } //user input
+
+
+                PrintMenu(select, menuText, w); //custom print method
+            }
+
+            switch (select)
+            {
+                case 0:
+                    ConsoleSetUp(w, h);
+                    DrawBackGround(h, w);
+                    RunGame(swarm, missles, upgrades, rng, matrix, tbBorder);
+                    break;
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Remains to be implemented");
+
+                    Console.ReadLine();
+                    goto Selection;
+                case 2:
+                    PrintHighScore();
+                    goto Selection;
+                case 3:
+                    return;
+                default:
+                    break;
+            }
+
+            //RunGame(swarm, missles, rng, matrix, tbBorder);
         }
+
+        static public void RunGame(List<Alien> swarm, List<Projectile> missles, List<Upgrade> upgrades, Random rng, int[,] matrix, int b)
+        {
+            string[] txtFile = System.IO.File.ReadAllLines("Level2.txt");
+            for (int i = 0; i < txtFile.Length; i++)
+            {
+                string[] temp = txtFile[i].Split(',');
+                swarm.Add(new Alien(int.Parse(temp[0]), int.Parse(temp[1]), int.Parse(temp[2]), rng.Next(20, 500)));
+            }
+
+            foreach (Alien ship in swarm)
+            {
+                ship.PlaceInGrid(matrix);
+            }
+            //string monitor = printGrid(matrix);
+            // Console.Write(monitor);
+            //Hero player = new Hero();
+            player.PlaceInGrid(matrix);
+            while (true)
+            {
+                int i = GameTurn(swarm, missles, upgrades, player, matrix, b);
+                if (i == 0)
+                {
+                    //=============================================================================================> LOSE SCREEN
+                    LoseScreen(29, 80);
+                    //=============================================================================================> LOSE SCREEN
+                    break;
+                }
+                if (swarm.Count == 0)
+                {//win
+                    Console.Clear();
+                    WinScreenNinja();
+                    break;
+                }
+              }
+
+            //Console.Clear();
+            //Console.WriteLine("Game Over");
+            //Console.WriteLine(player.score);
+
+            //HighScore(player);
+        }
+
+        static public int GameTurn(List<Alien> s, List<Projectile> p, List<Upgrade> upgrades, Hero h, int[,] grid, int b)
+        {
+
+            string monitor = printGrid(grid);
+            PrintMonitorLine(monitor, h, b);
+
+            //Fire
+            foreach (Alien ship in s)
+            {
+                upgrades.Add(ship.Hit_Detect(p, grid, h));
+                p.Add(ship.Fire());
+                p.RemoveAll(o => o == null);
+                ship.ProgressTime();                
+            }
+
+            s.RemoveAll(o => o.lives <= 0);
+            upgrades.RemoveAll(o => o == null);
+            
+            foreach (Upgrade alien in upgrades)
+            {
+                alien.PlaceInGrid(grid);
+                alien.MoveTimeProgress(grid);
+                alien.HeroCatch(h, grid);
+            }
+
+            upgrades.RemoveAll(o => o.type == 5);
+            
+            foreach (Upgrade alien in upgrades)
+            {
+                if (alien.x == grid.GetLength(0) - 1 || alien.x == 0)
+                {
+                    alien.direction *= -1;
+                    if (alien.y == grid.GetLength(1) - 1 || alien.y == 0)
+                    {
+                        alien.secondDirection *= -1;
+                    }
+                }
+            }
+
+            foreach (Projectile missle in p)
+            {
+                missle.PlaceInGrid(grid);
+            }
+            // 
+
+
+            foreach (Projectile missle in p)
+            {
+                if (missle.x == grid.GetLength(0) - 1 || missle.x == missle.speed-1)
+                {
+                    missle.RemoveFromGrid(grid);
+                }
+                if (missle.y == grid.GetLength(1) - 1 || missle.y == 0)
+                {
+                    missle.RemoveFromGrid(grid);
+                }
+            }
+
+            p.RemoveAll(o => o.x == grid.GetLength(0) - 1);
+            p.RemoveAll(o => o.x == 0);
+
+            p.RemoveAll(o => o.y == grid.GetLength(1) - 1);
+            p.RemoveAll(o => o.y == 0);
+            //
+            //s.RemoveAll(o => o.lives <= 0);
+            monitor = printGrid(grid);
+
+            foreach (Projectile missle in p)
+            {
+                missle.TimeProgress(grid);
+            }
+
+            HeroMove(h, grid, p);
+            h.Hit(p, grid);
+            if(s.Count<=2)
+            {
+                int j = 0;
+            }
+            Thread.Sleep(100);
+
+            if (h.lives > 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+       
     }
-}
--- AlienInvaders/AlienInvaders/Sound.cs
+++ AlienInvaders/AlienInvaders/Sound.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameVersion1
+{
+    partial class GameBody
+    {
+        static void HeroProjSoundZero()
+        {
+            Console.Beep(2000, 80);
+        }
+
+        static void HeroProjSoundOne()
+        {
+            Console.Beep(3000, 80);
+        }
+
+        static void HeroProjSoundTwo()
+        {
+            Console.Beep(4000, 80);
+        }
+    }
+}
